3d510fe8719404c8ac24b13267512eb68720a145
David V <davr59@outlook.com>
2022-03-13 08:39:39 -0600

    order switch by name asc

--- app.js
+++ app.js
@@ -4,26 +4,26 @@ const { LifxEffects } = require('./lifx-effects');
 const lifxEffects = new LifxEffects(config);
 const args = process.argv.slice(2);
 switch (args[0]) {
-  case 'cartagines_goal':
-    lifxEffects.runCartaginesGoalEffect();
-    break;
-  case 'cartagines_win':
-    lifxEffects.runCartaginesWinEffect();
-    break;
-  case 'xbox_on':
-    lifxEffects.runXboxOnEffect();
-    break;
-  case 'xbox_achievement':
-    lifxEffects.runXboxAchievementEffect();
-    break;
   case 'bitcoin_new_high':
     lifxEffects.runBitcoinNewHighEffect();
     break;
   case 'bitcoin_new_low':
     lifxEffects.runBitcoinNewLowEffect();
     break;
+  case 'cartagines_goal':
+    lifxEffects.runCartaginesGoalEffect();
+    break;
+  case 'cartagines_win':
+    lifxEffects.runCartaginesWinEffect();
+    break;
   case 'gallotapado':
     lifxEffects.runGalloTapadoEffect();
+    break;
+  case 'xbox_on':
+    lifxEffects.runXboxOnEffect();
+    break;
+  case 'xbox_achievement':
+    lifxEffects.runXboxAchievementEffect();
     break;
   case 'random_color':
     lifxEffects.runRandomColorEffect(args[1]);
